7fb91804fff05cddd0288dcf4ee3cecce81854a0
Aguibou <barrybagata96@gmail.com>
2022-09-23 16:13:08 +0200

    lier page detail a page liste des reservation

--- frontend/src/components/Bookings.vue
+++ frontend/src/components/Bookings.vue
@@ -13,121 +13,63 @@
             class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8"
           >
             <div
-              class="
-                overflow-hidden
-                shadow
-                ring-1 ring-black ring-opacity-5
-                md:rounded-lg
-              "
+              class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg"
             >
               <table class="min-w-full divide-y divide-gray-300">
                 <thead class="bg-gray-50">
                   <tr>
                     <th
                       scope="col"
-                      class="
-                        py-3.5
-                        pl-4
-                        pr-3
-                        text-left text-sm
-                        font-semibold
-                        text-gray-900
-                        sm:pl-6
-                      "
+                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                     >
                       ID
                     </th>
                     <th
                       scope="col"
-                      class="
-                        px-3
-                        py-3.5
-                        text-left text-sm
-                        font-semibold
-                        text-gray-900
-                      "
+                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
                       Vol
                     </th>
                     <th
                       scope="col"
-                      class="
-                        px-3
-                        py-3.5
-                        text-left text-sm
-                        font-semibold
-                        text-gray-900
-                      "
+                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
                       User
                     </th>
                     <th
                       scope="col"
-                      class="
-                        px-3
-                        py-3.5
-                        text-left text-sm
-                        font-semibold
-                        text-gray-900
-                      "
+                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
                       Aller/Retour
                     </th>
                     <th
                       scope="col"
-                      class="
-                        px-3
-                        py-3.5
-                        text-left text-sm
-                        font-semibold
-                        text-gray-900
-                      "
+                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
                       Option champagne
                     </th>
                     <th
                       scope="col"
-                      class="
-                        px-3
-                        py-3.5
-                        text-left text-sm
-                        font-semibold
-                        text-gray-900
-                      "
+                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
                       Montant
                     </th>
                     <th
                       scope="col"
-                      class="
-                        px-3
-                        py-3.5
-                        text-left text-sm
-                        font-semibold
-                        text-gray-900
-                      "
+                      class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
                       Date
                     </th>
 
                     <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                      <span class="sr-only">Voir Détails</span>
+                      <span class="sr-only"> Voir Détails</span>
                     </th>
                   </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200 bg-white">
                   <tr v-for="book in bookings" :key="book.id">
                     <td
-                      class="
-                        whitespace-nowrap
-                        py-4
-                        pl-4
-                        pr-3
-                        text-sm
-                        font-medium
-                        text-gray-900
-                        sm:pl-6
-                      "
+                      class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
                     >
                       {{ book.id }}
                     </td>
@@ -148,18 +90,10 @@
                         <input
                           aria-describedby="RetourInclus"
                           name="comments"
-                                                    disabled="true"
-
+                          disabled="true"
                           type="checkbox"
                           :checked="book.retour_inclut"
-                          class="
-                            h-4
-                            w-4
-                            rounded
-                            border-gray-300
-                            text-blue-600
-                            focus:ring-blue-500
-                          "
+                          class="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
                         />
                       </div>
                     </td>
@@ -173,14 +107,7 @@
                           disabled="true"
                           type="checkbox"
                           :checked="book.champagne"
-                          class="
-                            h-4
-                            w-4
-                            rounded
-                            border-gray-300
-                            text-blue-600
-                            focus:ring-blue-500
-                          "
+                          class="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
                         />
                       </div>
                     </td>
@@ -195,19 +122,14 @@
                       {{ book.date }}
                     </td>
                     <td
-                      class="
-                        relative
-                        whitespace-nowrap
-                        py-4
-                        pl-3
-                        pr-4
-                        text-right text-sm
-                        font-medium
-                        sm:pr-6
-                      "
+                      class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
                     >
-                      <a href="#" class="text-blue-600 hover:text-blue-900"
-                        >Voir<span class="sr-only">, {{ book.user }}</span></a
+                      <a
+                        :href="'/bookings/' + book.id"
+                        class="text-blue-600 hover:text-blue-900"
+                      >
+                        Voir
+                        <span class="sr-only">, {{ book.user }}</span></a
                       >
                     </td>
                   </tr>
@@ -235,5 +157,4 @@ export default {
   },
 };
 </script>
-<style lang="">
-</style>
+<style lang=""></style>

4f6f34bab09d88062ab41e0fc54393981b9410b4
NanonLevana <nanon.ouattara@levana.io>
2022-09-26 13:28:12 +0200

    broker post flights and bookings

--- backend/booking/booking_app/api/views.py
+++ backend/booking/booking_app/api/views.py
@@ -7,6 +7,18 @@ from rest_framework.response import Response
 from datetime import datetime
 from .utils import get_currency, str_to_bool
 
+provider_key = "2016939750"
+
+reservation_endpoint = {
+  "prof" : "https://api-6yfe7nq4sq-uc.a.run.app/book",
+  "team" : "http://server.aurelientorres.com/bookings/"
+}
+
+vol_endpoint = {
+  "prof" : "https://api-6yfe7nq4sq-uc.a.run.app/flights",
+  "team" : "http://server.aurelientorres.com/flights"
+}
+
 def build_vol(source, external_vol, currencies) :
 
   if  source == "prof" :
@@ -28,7 +40,7 @@ def build_vol(source, external_vol, currencies) :
                 "source" : source
               }
     return vol
-  elif  source == "team" :
+  else :
     montant = external_vol["price"]
 
     price_dict = {
@@ -38,83 +50,130 @@ def build_vol(source, external_vol, currencies) :
     for currency in currencies:
         price_dict[currency] = montant * float(currencies[currency])
     vol = {
-                "code" : external_vol["id"],
-                "depart" : external_vol["airport_start"],
-                "arrive" : external_vol["airport_arrival"],
+                "code" : external_vol["internal_code"],
+                "depart" : external_vol["departure"],
+                "arrive" : external_vol["arrival"],
                 "montant" : montant,
-                "places" : external_vol["nb_max_places"],
+                "places" : external_vol["total_seats"],
                 "price_map" : price_dict,
-                "source" : source
+                "source" : external_vol["tenant"]
               }
     return vol
 
+def build_booking(source, booking) :
+
+  payload = {}
+
+  if source == "prof":
+    vols = json.loads(requests.get(vol_endpoint[source]).content.decode('utf-8'))
+    vol = list(filter(lambda vol: vol["code"] == booking.get("code"), vols))
+
+    champagne = str_to_bool(booking.get('champagne'))
+    retour_inclut = str_to_bool(booking.get('retour_inclut'))
+    first_class = str_to_bool(booking.get('first_class'))
+
+    montant_vol = vol[0]["base_price"] * int(booking.get('nb_place'))
+
+    if champagne : montant_vol += 100
+
+    if first_class : montant_vol += montant_vol * 1.5
+
+    if retour_inclut : montant_vol *= 1.95
+
+    date = datetime.strptime(booking.get('date_depart'), "%Y-%m-%d")
+    date_str = date.strftime("%d-%m-%Y")
+    payload = {
+      "code": "None",
+      "flight": {
+          "code": vol[0]["code"],
+          "departure": vol[0]["departure"],
+          "arrival": vol[0]["arrival"],
+          "base_price": vol[0]["base_price"],
+          "plane": {
+              "name": vol[0]["plane"]["name"],
+              "total_seats": vol[0]["plane"]["total_seats"]
+          }
+      },
+      "date": date_str,
+      "payed_price": int(montant_vol),
+      "customer_name": booking.get('prenom') + " " + booking.get('nom'),
+      "customer_nationality": "",
+      "options": [],
+      "booking_source": "GROUP2"
+    }
+  else :
+    vols = json.loads(requests.get(vol_endpoint["team"]).content.decode('utf-8'))
+    vol = list(filter(lambda vol: vol["internal_code"] == booking.get("code") and vol["tenant"] == source, vols))
+
+    montant_vol = vol[0]["price"] * int(booking.get('nb_place'))
+
+    if champagne : montant_vol += 100
+
+    if first_class : montant_vol += montant_vol * 1.5
+
+    if retour_inclut : montant_vol *= 1.95
+
+    date = datetime.strptime(booking.get('date_depart'), "%Y-%m-%d")
+    date_str = date.strftime("%d/%m/%Y")
+
+    payload = {
+      "provider_key": provider_key,
+      "booking": {
+        "total_price": montant_vol,
+        "currency":
+        {
+            "currency_code": "EUR",
+            "currency_name": "euro"
+        },
+        "tickets":
+        [
+            {
+                "flight_code": vol["internal_code"],
+                "person":
+                {
+                    "first_name": booking.get('prenom'),
+                    "last_name" : booking.get('nom'),
+                    "birth_date": "None"
+                },
+                "price": 0,
+                "booking_date" : date_str,
+                "option_codes": []
+            }
+        ]
+      }
+    }
+  return payload
+
 class ExternalVol(APIView):
-
-    reservation_endpoint = {
-      "prof" : "https://api-6yfe7nq4sq-uc.a.run.app/book",
-      #"team" : "https://10.8.110.210:52880/Booking/"
-    }
-
-    vol_endpoint = {
-      "prof" : "https://api-6yfe7nq4sq-uc.a.run.app/flights",
-      #"team" : "https://10.8.110.210:52880/Flight/"
-    }
 
     def get(self, request, format=None):
 
         currencies = get_currency()
         vols = []
-        for endpoint in self.vol_endpoint :
+        for endpoint in vol_endpoint :
           result = None
           try:
-            result = requests.get(self.vol_endpoint[endpoint], verify=False, timeout=5)
+            result = requests.get(vol_endpoint[endpoint], verify=False, timeout=5)
           except :
             continue
           
           for external_vol in json.loads(result.content.decode('utf-8')):
-              
-              vols.append(build_vol(endpoint, external_vol, currencies))
+
+            if "tenant" in external_vol and external_vol["tenant"] == "GROUP2" : continue
+            vols.append(build_vol(endpoint, external_vol, currencies))
         
         return Response(vols)
 
     def post(self, request, format=None):
-      vols = json.loads(requests.get('https://api-6yfe7nq4sq-uc.a.run.app/flights').content.decode('utf-8'))
-      vol = list(filter(lambda vol: vol["code"] == request.data.get("code"), vols))
+      source = "prof" if request.data.get("source") == "prof" else "team"
 
-      champagne = str_to_bool(request.data.get('champagne'))
-      retour_inclut = str_to_bool(request.data.get('retour_inclut'))
-      first_class = str_to_bool(request.data.get('first_class'))
+      result = None
 
-      montant_vol = vol[0]["base_price"] * int(request.data.get('nb_place'))
-
-      if champagne : montant_vol += 100
-
-      if first_class : montant_vol += montant_vol * 1.5
-
-      if retour_inclut : montant_vol *= 1.95
-
-      date = datetime.strptime(request.data.get('date_depart'), "%Y-%m-%d")
-      date_str = date.strftime("%d-%m-%Y")
-      payload = {
-        "code": "None",
-        "flight": {
-            "code": vol[0]["code"],
-            "departure": vol[0]["departure"],
-            "arrival": vol[0]["arrival"],
-            "base_price": vol[0]["base_price"],
-            "plane": {
-                "name": vol[0]["plane"]["name"],
-                "total_seats": vol[0]["plane"]["total_seats"]
-            }
-        },
-        "date": date_str,
-        "payed_price": int(montant_vol),
-        "customer_name": request.data.get('prenom') + " " + request.data.get('nom'),
-        "customer_nationality": "",
-        "options": [],
-        "booking_source": "BookingVol"
-      }
-      
-      result = requests.post('https://api-6yfe7nq4sq-uc.a.run.app/book', json = payload)
+      payload = build_booking()
+        
+      try :
+        result = requests.post(self.reservation_endpoint[source], json = payload)
+      except:
+        result = "Something went wrong, booking failed."
 
       return Response(result)
